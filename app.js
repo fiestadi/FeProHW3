feb7c406139fd527059961ad66baec67838381ea
fiestadi <124197643+fiestadi@users.noreply.github.com>
2023-05-27 16:30:51 +0200

    hw3

---
+++ app.js
@@ -0,0 +1,33 @@
+//Написать программы, которая ищет на основе запроса пользователя (prompt) и изменяет найденное значение внутри массива.
+// example 1
+//  let arr = ['apple', 'ananas', 'peach', 'grape', 'plum'];
+// console.log("Fruits", arr);
+// let fruits = prompt("Enter a search fruit:");
+// for(let i =0; i < arr.length; i++){
+//    if(arr [i] == fruits){
+//       let newFruits = prompt("Enter new fruit ");
+//       arr[i] = newFruits }
+// }
+// console.log(arr);
+//  example2
+// let arr = [10, 20, 30, 40, 50 ,60, 70, 80,];
+// let searchNum = prompt("Enter some number:");
+// let newNum = prompt("Enter new number :");
+// for (let i = 0; i < arr.length; i++){
+//    if(arr[i] == searchNum){
+//       arr[i] = newNum;
+//    }
+// }
+// console.log(arr);
+
+// example3
+// let arr = ['Linda', 'Adela', 'Maja', 'Silvie'];
+// let searchName = prompt(" Enter female name:");
+// let newName = prompt( "Enter new female name :");
+// arr.forEach(function(iteam,index){
+//    if(iteam == searchName){
+//     arr[index] = newName;  
+//    }
+// });
+// console.log(arr);
+
